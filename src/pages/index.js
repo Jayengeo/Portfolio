78943b30eb48dad35141e6bc06ac72f03f7ad8b5
Jacky Ngo <jacky.ngo96@gmail.com>
2023-07-31 15:52:48 -0400

    Edit home page description

--- src/pages/index.js
+++ src/pages/index.js
@@ -25,8 +25,7 @@ export default function Home() {
               <p className="my-4 text-base font-medium">
                 As a skilled full-stack developer, I am dedicated to turning
                 ideas into innovative web applications. Explore my latest
-                projects and articles, showcasing my expertise in React.js and
-                web development.
+                projects showcasing my expertise in web development!
               </p>
             </div>
           </div>
