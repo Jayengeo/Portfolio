1fc99e6f3bc04d1ce7299b7126931cb6abdbb085
Jacky Ngo <jacky.ngo96@gmail.com>
2023-08-15 09:12:26 -0400

    add basic biography

--- src/pages/about.js
+++ src/pages/about.js
@@ -11,18 +11,23 @@ const about = () => {
         <meta name="description" content="any description" />
       </Head>
       <main className="flex w-full flex-col items-center justify-center">
-        <Layouts className="pt-16"></Layouts>
-        <AnimatedText text="Did you know?" />
-        <div className="grid w-full grid-cols-8 gap-16">
-          <div className="col-span-3 flex flex-col items-start justify-start">
-            <h2 className="mb-4 text-lg font-bold uppercase text-dark/75">
-              Biography
-            </h2>
-            Hello! I'm Jacky 😃 I'm a Fullstack Developer who loves design and technology. I have a passion for developing aesthetically pleasing and intuitive applications. In my spare time, I love to rock climb 🧗 and spend time with my dog Toshi 🐕
+        <Layouts className="pt-16">
+          <AnimatedText text="Did you know?" className="mb-16" />
+          <div className="grid w-full grid-cols-8 gap-16">
+            <div className="col-span-3 flex flex-col items-start justify-start">
+              <h2 className="mb-4 text-lg font-bold uppercase text-dark/75">
+                Biography
+              </h2>
+              <p className="font-medium">
+                Hello! I'm Jacky 😃 I'm a Fullstack Developer who loves design
+                and technology. I have a passion for developing aesthetically
+                pleasing and intuitive applications. In my spare time, I love to
+                rock climb 🧗 and spend time with my dog Toshi 🐕
+              </p>
+            </div>
           </div>
-        </div>
+        </Layouts>
       </main>
-      
     </>
   );
 };
