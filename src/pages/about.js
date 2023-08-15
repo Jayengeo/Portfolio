f46b5996dccf7edc90e2717890a1b6fa03e7df30
Jacky Ngo <jacky.ngo96@gmail.com>
2023-08-15 14:28:42 -0400

    add and edit biography content

--- src/pages/about.js
+++ src/pages/about.js
@@ -15,18 +15,27 @@ const about = () => {
         <Layouts className="pt-16">
           <AnimatedText text="Did you know?" className="mb-16" />
           <div className="grid w-full grid-cols-8 gap-16">
-            <div className="col-span-4 flex flex-col items-start justify-start">
+            <div className="col-span-5 flex flex-col items-start justify-start">
               <h2 className="mb-4 text-lg font-bold uppercase text-dark/75">
                 Biography
               </h2>
               <p className="font-medium">
-                Hello! I'm Jacky 😃 I'm a Fullstack Developer who loves design
-                and technology. I have a passion for developing aesthetically
-                pleasing and intuitive applications. In my spare time, I love to
+                Hello! I'm Jacky 😃 I'm a Fullstack Developer who loves all things design 🎨
+                and technology 🤖. I have a background in Architecture and contrsuction management 🏠 but my most recent endevor was in Tech sales and client success 📈
+              </p>
+              
+              <p className="my-4 font-medium">
+              I have a passion for developing aesthetically
+                pleasing and intuitive applications 📱.
+              </p> 
+
+              <p className="my-4 font-medium">
+              In my spare time, I love to
                 rock climb 🧗 and spend time with my dog Toshi 🐕
               </p>
+            
             </div>
-            <div className="col-span-4 relative h-max rounded-2xl border-2 border-solid border-dark bg-light p-8">
+            <div className="col-span-3 relative h-max rounded-2xl border-2 border-solid border-dark bg-light p-8">
               <div className="absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark" />
 
               <Image
