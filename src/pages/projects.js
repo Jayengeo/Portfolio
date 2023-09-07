6cc49479e024cbb03731078094fb1e5b07a95ba8
Jacky Ngo <jacky.ngo96@gmail.com>
2023-09-07 16:01:00 -0400

    style featured project

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -7,23 +7,35 @@ import Image from "next/image";
 import { GithubIcon } from "@/components/Icons";
 import project1 from "../../public/images/projects/crypto-screener-cover-image.jpg";
 
-const FeaturedProject = ({type, title, summary, img, link, github}) => {
+const FeaturedProject = ({ type, title, summary, img, link, github }) => {
   return (
-    <article>
-      <Link href={link} target="_blank">
+    <article className=" w-full flex items-center justify-between rounded-3xl border border-solid border-dark bg-light shadow-2xl p-12">
+      <Link
+        className="w-1/2 cursor-pointer overflow-hidden rounded-lg"
+        href={link}
+        target="_blank"
+      >
         <Image src={img} alt={title} className="w-full h-auto" />
       </Link>
-      <div>
-        <span>{type}</span>
-        <Link href={link} target="_blank">
-          <h2>{title}</h2>
+      <div className="w-1/2 flex flex-col items-start justify-between pl-6">
+        <span className="text-primary font-medium text-xl">{type}</span>
+        <Link
+          href={link}
+          target="_blank"
+          className="hover:underline underline-offset-2"
+        >
+          <h2 className="my-2 w-full text-left text-4xl font-bold">{title}</h2>
         </Link>
-        <p>{summary}</p>
-        <div>
-          <Link href={github} target="_blank">
+        <p className="my-2 font-medium text-dark">{summary}</p>
+        <div className="mt-2 flex items-center">
+          <Link href={github} target="_blank" className="w-10">
             <GithubIcon />
           </Link>
-          <Link href={link} target="_blank">
+          <Link
+            href={link}
+            target="_blank"
+            className="ml-4 rounded-lg bg-dark text-light p2 px-6 text-lg font-semibold"
+          >
             Visit Project
           </Link>
         </div>
@@ -41,7 +53,7 @@ const projects = () => {
       </Head>
       <main className="w-full mb-16 flex flex-col items-center justify-center">
         <Layouts className="pt-16">
-          <AnimatedText text="Check this out" />
+          <AnimatedText text="Check this out" className="mb-16" />
           <div className="grid grid-cols-12 gap-24">
             <div className="col-span-12 ">
               <FeaturedProject
