805b056b21c1e0f5891da9469bd2f6c4bdcd004f
Jacky Ngo <jacky.ngo96@gmail.com>
2023-11-02 15:16:58 -0400

    implement darkmode styling in navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -13,9 +13,9 @@ const CustomLink = ({ href, title, className = "" }) => {
     <Link href={href} className={`${className} relative group`}>
       {title}
       <span
-        className={`h-[1px] inline-block bg-dark absolute left-0 -bottom-0.5 group-hover:w-full transition-[width] ease duration-300 ${
-          router.asPath === href ? "w-full" : "w-0"
-        }`}
+        className={`h-[1px] inline-block bg-dark absolute left-0 -bottom-0.5 group-hover:w-full transition-[width] ease duration-300 
+        ${router.asPath === href ? "w-full" : "w-0"}
+        dark:bg-light`}
       >
         &nbsp;
       </span>
@@ -27,7 +27,7 @@ const NavBar = () => {
   const [mode, setMode] = useThemeSwitcher();
 
   return (
-    <header className="w-full px-32 py-8 font-medium flex items-center justify-between">
+    <header className="w-full px-32 py-8 font-medium flex items-center justify-between dark:text-light ">
       <nav>
         <CustomLink href="/" title="Home" className="mr-4" />
         <CustomLink href="about" title="About" className="mx-4" />
