3a87e191f98a9b5aaa1e7145230306e8ec952c90
Jacky Ngo <jacky.ngo96@gmail.com>
2023-11-02 15:26:56 -0400

    style darkmode button to show white background when in dark mode and black in lightmode

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -56,7 +56,7 @@ const NavBar = () => {
 
         <button
           onClick={() => setMode(mode === "light" ? "dark" : "light")}
-          className="ml-3 flex items-center justify-center rounded-full p-1"
+          className={`ml-3 flex items-center justify-center rounded-full p-1 ${mode === "light" ? "bg-dark text-light" : "bg-light text-dark"} `}
         >
           {mode === "dark" ? (
             <SunIcon className={"fill-dark"} />
