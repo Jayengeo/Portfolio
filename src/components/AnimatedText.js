1c547255b8dc5cc3b23438e8660ff9638b87332e
Jacky Ngo <jacky.ngo96@gmail.com>
2023-11-02 15:33:51 -0400

    style text to appear white when in dark mode

--- src/components/AnimatedText.js
+++ src/components/AnimatedText.js
@@ -30,14 +30,14 @@ const singleWord = {
 
 const AnimatedText = ({ text, className = "" }) => {
   return (
-    <div className="w-full mx-auto py-2 flex items-center justify-center text-center overflow-hidden">
+    <div className="w-full mx-auto py-2 flex items-center justify-center text-center overflow-hidden ">
       <motion.h1
-        className={`inline-block w-full text-dark font-bold capitalize text-8xl ${className} `}
+        className={`inline-block w-full text-dark font-bold capitalize text-8xl dark:text-light ${className} `}
         variants={quote}
         initial="initial"
         animate="animate"
       >
-        {text.split(" ").map((word, index) =>
+        {text.split(" ").map((word, index) => (
           <motion.span
             key={word + "-" + index}
             className="inline-block"
@@ -45,7 +45,7 @@ const AnimatedText = ({ text, className = "" }) => {
           >
             {word}&nbsp;
           </motion.span>
-        )}
+        ))}
       </motion.h1>
     </div>
   );
