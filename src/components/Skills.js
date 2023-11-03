de00aa63e70d3aa1561050f7a1163fd867d9d341
Jacky Ngo <jacky.ngo96@gmail.com>
2023-11-02 20:24:27 -0400

    style projects page to have white borders and text when dark mode is turned on

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -7,8 +7,7 @@ const Skill = ({ name, x, y }) => {
       className="flex items-center justify-center rounded-full font-semibold bg-dark text-light py-3 px-6 shadow-dark cursor-pointer absolute dark:text-dark dark:bg-light "
       whileHover={{ scale: 1.05 }}
       initial={{ x: 0, y: 0 }}
-      whileInView={{ x: x, y: y }}
-      transition={{ duration: 1.5 }}
+      whileInView={{ x: x, y: y, transition:{ duration: 1.5 }}}
       viewport={{ once: true }}
     >
       {name}
