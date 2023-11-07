9bd05450693fb0caf4ca645f28a9038bd975b620
Jacky Ngo <jacky.ngo96@gmail.com>
2023-11-07 08:20:16 -0500

    edit column span of image and paragraph to fill the screen in medium screen format

--- src/pages/about.js
+++ src/pages/about.js
@@ -16,8 +16,8 @@ const about = () => {
       <main className="flex w-full flex-col items-center justify-center dark:text-light">
         <Layouts className="pt-16">
           <AnimatedText text="Did you know?" className="mb-16 lg:!text-7x1 sm:!text-6xl xs:!text-4xl sm:mb-8" />
-          <div className="grid w-full grid-cols-8 gap-16">
-            <div className="col-span-5 flex flex-col items-start justify-start">
+          <div className="grid w-full grid-cols-8 gap-16 sm:gap-8">
+            <div className="col-span-5 flex flex-col items-start justify-start xl:col-span-4 md:order-2 md:col-span-8">
               <h2 className="mb-4 text-lg font-bold uppercase text-dark/75">
                 Biography
               </h2>
@@ -38,7 +38,7 @@ const about = () => {
                 dog Toshi 🐕
               </p>
             </div>
-            <div className="col-span-3 relative h-max rounded-2xl border-2 border-solid border-dark bg-light p-8 dark:bg-dark dark:border-light">
+            <div className="col-span-3 relative h-max rounded-2xl border-2 border-solid border-dark bg-light p-8 dark:bg-dark dark:border-light xl:col-span-4 md:order-1 md:col-span-8">
               <div className="absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark dark:bg-light" />
 
               <Image
