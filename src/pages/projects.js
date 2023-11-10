0c8c591e6677fa68f71db2cce3d5a4cd7e0d5090
Jacky Ngo <jacky.ngo96@gmail.com>
2023-11-09 23:53:10 -0500

    adjust border of regular projects to be responsive

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -63,8 +63,8 @@ const FeaturedProject = ({ type, title, summary, img, link, github }) => {
 
 const Project = ({ type, title, summary, img, link, github }) => {
   return (
-    <article className="w-full flex flex-col item-center justify-center rounded-2xl border border-solid border-dark bg-light p-6 relative dark:bg-dark dark:border-light">
-      <div className="absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2rem] bg-dark rounded-br-3xl dark:bg-light" />
+    <article className="w-full flex flex-col item-center justify-center rounded-2xl border border-solid border-dark bg-light p-6 relative dark:bg-dark dark:border-light xs:p-4">
+      <div className="absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2rem] bg-dark rounded-br-3xl dark:bg-light md:-right-2 md:w-[101%] xs:h-[102%] xs:rounded-[1.5rem]" />
 
       <Link
         className="w-full cursor-pointer overflow-hidden rounded-lg"
@@ -80,7 +80,7 @@ const Project = ({ type, title, summary, img, link, github }) => {
         />
       </Link>
       <div className="w-full flex flex-col items-start justify-between mt-4">
-        <span className="text-primary font-medium text-xl dark:text-primaryDark">
+        <span className="text-primary font-medium text-xl dark:text-primaryDark lg">
           {type}
         </span>
         <Link
