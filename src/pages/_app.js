3069187d4f493dfaccc4a1507e4bfcdf3f8118f1
Jacky Ngo <jacky.ngo96@gmail.com>
2023-11-11 23:13:55 -0500

    add exit transition animation

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,6 +2,8 @@ import "@/styles/globals.css";
 import { Montserrat } from "next/font/google";
 import Head from "next/head";
 import NavBar from "@/components/NavBar";
+import { AnimatePresence } from "framer-motion";
+import { useRouter } from "next/router";
 
 const montserrat = Montserrat({
   subsets: ["latin"],
@@ -9,15 +11,20 @@ const montserrat = Montserrat({
 });
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
   return (
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={`${montserrat.variable} font-mont bg-light dark:bg-dark w-full min-h-screen`}>
-        <NavBar/>
-        <Component {...pageProps} />
+      <main
+        className={`${montserrat.variable} font-mont bg-light dark:bg-dark w-full min-h-screen`}
+      >
+        <NavBar />
+        <AnimatePresence>
+          <Component key={router.asPath} {...pageProps} />
+        </AnimatePresence>
       </main>
     </>
   );
