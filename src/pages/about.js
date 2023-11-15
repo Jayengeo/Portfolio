9e6913f5d0e3249360ca0104431a73b656bc5372
Jacky Ngo <jacky.ngo96@gmail.com>
2023-11-15 09:27:56 -0500

    add third image to about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,6 +4,7 @@ import AnimatedText from "../components/AnimatedText";
 import Layouts from "../components/Layouts";
 import About1 from "../../public/images/profile/Mind map-pana.png";
 import About2 from "../../public/images/profile/Construction costs-pana.png";
+import About3 from "../../public/images/profile/Webinar-pana.png";
 import Image from "next/image";
 import Skills from "../components/Skills";
 import TransitionEffect from "../components/TransitionEffect";
@@ -62,6 +63,18 @@ const about = () => {
 
               <Image
                 src={About2}
+                alt="jacky"
+                className="w-full h-auto rounded-2xl"
+                priority
+                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+              />
+            </div> 
+
+            <div className="col-span-3 relative h-max rounded-2xl border-2 border-solid border-dark bg-light  dark:bg-dark dark:border-light xl:col-span-4 md:order-1 md:col-span-8">
+              <div className="absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark dark:bg-light" />
+
+              <Image
+                src={About3}
                 alt="jacky"
                 className="w-full h-auto rounded-2xl"
                 priority
