e29d78cdfaea2c5fafa8a341df2840e9da1a80d6
Jacky Ngo <jacky.ngo96@gmail.com>
2023-11-15 09:04:12 -0500

    add second image in about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,7 +2,8 @@ import React from "react";
 import Head from "next/head";
 import AnimatedText from "../components/AnimatedText";
 import Layouts from "../components/Layouts";
-import ProfilePic from "../../public/images/profile/Mind map-pana.png";
+import About1 from "../../public/images/profile/Mind map-pana.png";
+import About2 from "../../public/images/profile/Construction costs-pana.png";
 import Image from "next/image";
 import Skills from "../components/Skills";
 import TransitionEffect from "../components/TransitionEffect";
@@ -14,10 +15,13 @@ const about = () => {
         <title>Jacky| About Page</title>
         <meta name="description" content="any description" />
       </Head>
-      <TransitionEffect/>
+      <TransitionEffect />
       <main className="flex w-full flex-col items-center justify-center dark:text-light">
         <Layouts className="pt-16">
-          <AnimatedText text="A little bit about me" className="mb-16 lg:!text-7x1 sm:!text-6xl xs:!text-4xl sm:mb-8" />
+          <AnimatedText
+            text="A little bit about me"
+            className="mb-16 lg:!text-7x1 sm:!text-6xl xs:!text-4xl sm:mb-8"
+          />
           <div className="grid w-full grid-cols-8 gap-16 sm:gap-8">
             <div className="col-span-5 flex flex-col items-start justify-start xl:col-span-4 md:order-2 md:col-span-8">
               <h2 className="mb-4 text-lg font-bold uppercase text-dark/75">
@@ -27,7 +31,8 @@ const about = () => {
                 Hello! Im Jacky 😃 Im a Fullstack Developer who loves all things
                 design 🎨 and technology 🤖. I have a background in Architecture
                 and contrsuction management 🏠 but my most recent endevor was in
-                Tech sales and client success 📈 Im currently an employee at Apple store and I hope to utilize my skills in a developer role.
+                Tech sales and client success 📈 Im currently an employee at
+                Apple store and I hope to utilize my skills in a developer role.
               </p>
 
               <p className="my-4 font-medium">
@@ -44,13 +49,20 @@ const about = () => {
               <div className="absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark dark:bg-light" />
 
               <Image
-                src={ProfilePic}
+                src={About1}
                 alt="jacky"
                 className="w-full h-auto rounded-2xl"
                 priority
                 sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
               />
             </div>
+            <Image
+              src={About2}
+              alt="jacky"
+              className="w-full h-auto rounded-2xl"
+              priority
+              sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+            />
             <div>
               {/* <div>
                 <span>50+</span>
