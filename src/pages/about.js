6252620c641b36f975f17526f5164d3dee68e86f
Jacky Ngo <jacky.ngo96@gmail.com>
2023-11-23 15:58:04 -0500

    create carousel for biography

--- src/pages/about.js
+++ src/pages/about.js
@@ -9,39 +9,30 @@ import About3 from "../../public/images/profile/Telecommuting-pana.png";
 import Image from "next/image";
 import Skills from "../components/Skills";
 import TransitionEffect from "../components/TransitionEffect";
+import Carousel from "../components/Carousel.js";
 
-const Biography = ({ summary, img, side }) => {
-  if (side === "right") {
-    return (
-      <>
-        <Image
-          src={img}
-          alt="jacky"
-          className="col-span-5 h-50% xl:col-span-4 md:col-span-8 "
-          priority
-          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-        />
-        <p className="flex items-center col-span-5 font-medium text-2xl xl:col-span-4 md:col-span-8">
-          {summary}
-        </p>
-      </>
-    );
-  }
+const Biography = ({ summary, img }) => {
   return (
     <>
-      <p className="flex items-center col-span-5 font-medium text-2xl xl:col-span-4 md:col-span-8">
-        {summary}
-      </p>
       <Image
         src={img}
         alt="jacky"
         className="col-span-5 h-50% xl:col-span-4 md:col-span-8 "
         priority
-        sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
       />
+      <p className="flex items-center col-span-5 font-medium text-2xl xl:col-span-4 md:col-span-8">
+        {summary}
+      </p>
     </>
   );
 };
+
+const slides = [
+  {"img":About1,"par":"hello"},
+  {"img":About2,"par":"hello"},
+  {"img":About3,"par":"hello"},
+  {"img":About4,"par":"hello"},
+];
 
 const about = () => {
   return (
@@ -57,38 +48,42 @@ const about = () => {
             text="A little bit about me"
             className="mb-16 lg:!text-7x1 sm:!text-6xl xs:!text-4xl sm:mb-8"
           />
-          <div className="grid w-full grid-cols-10 gap-1 sm:gap-8">
-            <Biography
-              summary="Hello! My name is Jacky, Im a Fullstack Developer who has a
+          <div className="flex justify-center max-w-lg">
+            <Carousel>
+              {slides.map((bio,index) => (
+                <Image key={index} src={bio.img} alt={`Slide ${index}`} />
+            
+              ))}
+            </Carousel>
+          </div>
+          {/* <Biography
+            summary="Hello! My name is Jacky, Im a Fullstack Developer who has a
                 passion for developing aesthetic and intuitive web appications"
-              img={About1}
-              side={"right"}
-            />
-            <Biography
-              summary="I have a background in architecture and construction management while in post secondary I
+            img={About1}
+            side={"right"}
+          />
+          <Biography
+            summary="I have a background in architecture and construction management while in post secondary I
               got some hands on experience working for organizations such as
               Mattamy Homes as a purchaser and as a assitant project manager
               with the city of mississauga"
-              img={About2}
-              side={"left"}
-            />
-            <Biography
-              summary="Upon graduating, I had the oportunity to work for a remote
+            img={About2}
+          />
+          <Biography
+            summary="Upon graduating, I had the oportunity to work for a remote
               techstart where I worked in tech sales, later transitioning to a
               client success manager"
-              img={About3}
-              side={"right"}
-            />
-            <Biography
-              summary=" I originally studied architecture while in post secondary and
+            img={About3}
+          />
+          <Biography
+            summary=" I originally studied architecture while in post secondary and
               got some hands on experience working for organizations such as
               Mattamy Homes as a purchaser and as a assitant project manager
               with the city of mississauga"
-              img={About4}
-              side={"left"}
-            />
-          </div>
-          <Skills />
+            img={About4}
+          />
+
+          <Skills /> */}
         </Layouts>
       </main>
     </>
