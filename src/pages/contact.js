2dd01afa28e3f62de47d46b52548d866a91c471c
Jacky Ngo <jacky.ngo96@gmail.com>
2023-11-28 23:43:29 -0500

    install and add emailjs to contact component

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,12 +1,35 @@
-import React from "react";
+
 import Head from "next/head";
 import Layouts from "../components/Layouts";
 import Image from "next/image";
 import profilePic from "../../public/images/profile/Programming-pana.png";
 import AnimatedText from "../components/AnimatedText";
 import TransitionEffect from "../components/TransitionEffect";
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
 
 const contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "YOUR_SERVICE_ID",
+        "YOUR_TEMPLATE_ID",
+        form.current,
+        "YOUR_PUBLIC_KEY"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
   return (
     <>
       <Head>
@@ -16,6 +39,16 @@ const contact = () => {
       <TransitionEffect />
       <main className="flex items-center text-dark w-full min-h-screen dark:text-light 2xl:">
         <Layouts className="pt-0 md:pt-16 sm:pt-8"></Layouts>
+
+        <form ref={form} onSubmit={sendEmail}>
+          <label>Name</label>
+          <input type="text" name="user_name" />
+          <label>Email</label>
+          <input type="email" name="user_email" />
+          <label>Message</label>
+          <textarea name="message" />
+          <input type="submit" value="Send" />
+        </form>
       </main>
     </>
   );
