f8fca54479802ae87d962ee35f5821fe59a47805
Jacky Ngo <jacky.ngo96@gmail.com>
2023-12-01 22:58:22 -0500

    add additional styling to input tags

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -21,6 +21,7 @@ const Contact = () => {
         (result) => {
           console.log(result.text);
           console.log("message sent");
+          e.target.reset();
         },
         (error) => {
           console.log(error.text);
@@ -37,25 +38,26 @@ const Contact = () => {
       <TransitionEffect />
       <main className="flex items-center text-dark w-full min-h-screen dark:text-light 2xl:">
         <Layouts className="pt-0 md:pt-16 sm:pt-8">
-          <form ref={form} onSubmit={sendEmail} className="w-400">
+          <form ref={form} onSubmit={sendEmail} className="w-400 ">
+            
             <label className="mt-4">Name</label>
             <input
               type="text"
               name="user_name"
-              className="w-full h-35 p-7 outline-none rounded-md border border-gray-300 focus:border-teal-500"
+              className=" w-full relative rounded-br-2x1 rounded-3xl border border-solid border-dark bg-light shadow-2xl p-12 dark:bg-dark dark:border-light"
             />
-
+          
             <label className="mt-4">Email</label>
             <input
               type="email"
               name="user_email"
-              className="w-full h-35 p-7 outline-none rounded-md border border-gray-300 focus:border-teal-500"
+              className="w-full relative rounded-br-2x1 rounded-3xl border border-solid border-dark bg-light shadow-2xl p-12 dark:bg-dark dark:border-light"
             />
 
             <label className="mt-4">Message</label>
             <textarea
               name="message"
-              className="w-full max-h-100 min-h-100 p-7 outline-none rounded-md border border-gray-300 focus:border-teal-500"
+              className="w-full relative rounded-br-2x1 rounded-3xl border border-solid border-dark bg-light shadow-2xl p-12 dark:bg-dark dark:border-light"
             />
 
             <input
