f21d67b5d33c85ec5cbadc670f7d1719b4e2dc5b
Jacky Ngo <jacky.ngo96@gmail.com>
2023-12-09 22:28:42 -0500

    move typescript icon and adjust padding for skills in skill tree

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -17,7 +17,7 @@ import { SiExpress } from "react-icons/si";
 const Skill = ({ name, icon, x, y }) => {
   return (
     <motion.div
-      className="flex items-center justify-center rounded-full font-semibold bg-dark text-light py-3 px-6 shadow-dark cursor-pointer absolute dark:text-dark dark:bg-light lg:py-2 lg:px-4 md:text-sm md:py-1.5 md:px-3 xs:bg-transparent xs:dark:bg-transparent xs:text-dark xs:dark:text-light xs:font-bold "
+      className="flex items-center justify-center rounded-full font-semibold bg-dark text-light py-3 px-2 shadow-dark cursor-pointer absolute dark:text-dark dark:bg-light lg:py-2 lg:px-4 md:text-sm md:py-1.5 md:px-3 xs:bg-transparent xs:dark:bg-transparent xs:text-dark xs:dark:text-light xs:font-bold "
       whileHover={{ scale: 1.05 }}
       initial={{ x: 0, y: 0 }}
       whileInView={{ x: x, y: y, transition: { duration: 1.5 } }}
@@ -53,7 +53,7 @@ const Skills = () => {
         <Skill name="Ruby" icon={<DiRuby />} x="15vw" y="15vw" />
         <Skill name="Tailwind CSS" icon={<SiTailwindcss />} x="-25vw" y="15vw" />
         <Skill name="NodeJS" icon={<FaNodeJs />} x="0vw" y="-20vw" />
-        <Skill name="Typescript" icon={<SiTypescript />} x="35vw" y="15vw" />
+        <Skill name="Typescript" icon={<SiTypescript />} x="8vw" y="0vw" />
         <Skill name="Express" icon={<SiExpress />} x="-30vw" y="-10vw" />
         <Skill name="Python" icon={<FaPython />} x="-0vw" y="-5vw" />
       </div>
