b5422bf25fc22d6045dd805a888554b9c17ab226
Jacky Ngo <jacky.ngo96@gmail.com>
2023-12-16 08:19:03 -0500

    space input fields in contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -66,20 +66,20 @@ const Contact = () => {
               <input
                 type="text"
                 name="user_name"
-                className="w-full rounded-br-2x1 rounded-3xl border-2 border-solid border-dark bg-light  p-6 dark:bg-dark dark:border-light mb-4"
+                className="w-full rounded-br-2x1 rounded-3xl border-2 border-solid border-dark bg-light  p-6 dark:bg-dark dark:border-light my-4"
               />
 
               <label className="mt-4">Email</label>
               <input
                 type="email"
                 name="user_email"
-                className="w-full rounded-br-2x1 rounded-3xl border-2 border-solid border-dark bg-light p-6 dark:bg-dark dark:border-light mb-4"
+                className="w-full rounded-br-2x1 rounded-3xl border-2 border-solid border-dark bg-light p-6 dark:bg-dark dark:border-light my-4"
               />
 
               <label className="mt-4">Message</label>
               <textarea
                 name="message"
-                className="w-full rounded-br-2x1 rounded-3xl border-2 border-solid border-dark bg-light p-12 dark:bg-dark dark:border-light mb-4"
+                className="w-full rounded-br-2x1 rounded-3xl border-2 border-solid border-dark bg-light p-12 dark:bg-dark dark:border-light my-4"
               />
               <div className="flex justify-center">
                 <motion.div
