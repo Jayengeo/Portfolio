8ce5654954786de7b5346c29f69ff934a39f8eec
Jacky Ngo <jacky.ngo96@gmail.com>
2023-12-17 22:02:02 -0500

    add icons to mobile menu

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -30,7 +30,7 @@ const CustomLink = ({ href, title, className = "", icon }) => {
   );
 };
 
-const CustomMobileLink = ({ href, title, className = "", toggle }) => {
+const CustomMobileLink = ({ href, title, className = "", toggle, icon }) => {
   const router = useRouter();
 
   const handleClick = () => {
@@ -44,7 +44,10 @@ const CustomMobileLink = ({ href, title, className = "", toggle }) => {
       className={`${className} relative group text-light dark:text-dark my-2`}
       onClick={handleClick}
     >
-      {title}
+      <div className="flex justify-between items-center ">
+        <div className="mr-2">{icon}</div>
+        <div>{title}</div>
+      </div>
       <span
         className={`h-[1px] inline-block bg-light dark:bg-dark absolute left-0 -bottom-0.5 group-hover:w-full transition-[width] ease duration-300 
         ${router.asPath === href ? "w-full" : "w-0"}
@@ -159,24 +162,28 @@ const NavBar = () => {
               href="/"
               title="Home"
               className=""
+              icon={<GrHomeRounded />}
               toggle={handleClick}
             />
             <CustomMobileLink
               href="/about"
               title="About"
               className=""
+              icon={<BsQuestionSquare />}
               toggle={handleClick}
             />
             <CustomMobileLink
               href="/projects"
               title="Projects"
               className=""
+              icon={<FaCode />}
               toggle={handleClick}
             />
             <CustomMobileLink
               href="/contact"
               title="contact"
               className=""
+              icon={<MdOutlineMail />}
               toggle={handleClick}
             />
           </nav>
