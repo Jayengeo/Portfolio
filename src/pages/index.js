d34e53c96d6153912e882c1c80e5c6953894ae78
Jacky Ngo <jacky.ngo96@gmail.com>
2024-04-25 16:02:47 -0400

    make homepage paragraph larger

--- src/pages/index.js
+++ src/pages/index.js
@@ -31,7 +31,7 @@ export default function Home() {
                 className="!text-6x1 !text-left
                 xl:!text-5xl lg:!text-center lg:!text=6xl md:!text-5xl sm:!text-3xl"
               />
-              <p className="my-4 text-base font-medium md:text-sm sm:text-xs">
+              <p className="my-4 text-base font-medium text-xl md:text-sm sm:text-xs">
                 As a skilled full-stack developer, I am dedicated to turning
                 ideas into innovative web applications. Explore my latest
                 projects showcasing my expertise in web development!
